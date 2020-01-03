ad187c8d8e5442be1c41ff6b83c98be741cf7d27
JOHNTECH <ajohnpaulantony@gmail.com>
2020-01-03 10:41:52 +0530

    Update CourseController.java

--- src/main/java/com/smi/innothink/CourseController.java
+++ src/main/java/com/smi/innothink/CourseController.java
@@ -1,7 +1,6 @@
 package com.smi.innothink;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
